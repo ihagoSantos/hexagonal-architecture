6a2c710e9948605290de82785b96fdd6159a629b
PS025684 <ps025684@corp.assai.com.br>
2023-12-19 16:53:31 -0300

    feat: init repository

---
+++ src/app/menu/menuPrincipal.ts
@@ -0,0 +1,22 @@
+import TerminalUtil from "@/util/TerminalUtil";
+import { terminal } from "terminal-kit";
+import menuFundamentos from "./menuFundamentos";
+
+export async function menuPrincipal() {
+    TerminalUtil.titulo('Menu Principal')
+
+    const resposta = await terminal.singleColumnMenu([
+        '1. Fundamentos',
+        'Sair'
+    ]).promise
+
+    switch (resposta.selectedIndex) {
+        case 0: await menuFundamentos(); break;
+        case 1: process.exit(0)
+    
+        default:
+            break;
+    }
+
+    menuPrincipal()
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { menuPrincipal } from "./app/menu/menuPrincipal"
+
+export function somar(a: number, b: number) {
+    return a + b
+}
+
+menuPrincipal()
